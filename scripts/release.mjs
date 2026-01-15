00868eac97e44da5be5965fa0e2be15a6d5174f0
Jin Soo Lim <jinsoo601@gmail.com>
2026-01-15 14:20:51 +0900

    fix release script

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -164,7 +164,7 @@ const buildTasks = (options) => {
           done: (otp) =>
             execa('npm', [
               'publish',
-              `./dist/pinginc-watermelondb-v${version}.tgz`,
+              `./dist/timebyping-watermelondb-v${version}.tgz`,
               `--otp=${otp}`,
               '--tag',
               tag,
